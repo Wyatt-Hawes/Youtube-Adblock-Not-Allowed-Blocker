449bf87d592c3db0683cc21ae651b7ab0b294653
Wyatt H <jhawes@ucsc.edu>
2023-10-04 20:01:36 -0700

    First iteration of adding a total blocked counter

--- content_scripts.js
+++ content_scripts.js
@@ -1,8 +1,12 @@
-console.log("YAB not allowed script is being run.")
+console.log("YAB not allowed script running");
 
 function block_popup_with_text(text_to_find){
     // Grab all pop-up elements on the page
+    //console.log("-----------------------------")
+    //console.log("Grabbing pop-up elements with the class of 'style-scope ytd-app'");
     const popup_elements = document.querySelectorAll('.style-scope.ytd-app');
+    //console.log("Found: " + popup_elements.length);
+
 
     //Loop through found elements
     for (const popup of popup_elements){
@@ -10,16 +14,50 @@ function block_popup_with_text(text_to_find){
         if (text_in_element.includes(text_to_find)){
             //Remove popup
             popup.remove();
-            console.log("Popup removed!")
+            //console.log("Popup removed!")
+
+            //Increment the number of blocked popups
+            let total_blocked = 0;
+            //returns null if item doesn't exist
+            if (localStorage["total_blocked"] != null){
+                total_blocked = parseInt(localStorage["total_blocked"])
+            }
+            total_blocked++;
+            localStorage["total_blocked"] = total_blocked;
+            console.log("Total Popups Blocked: " + localStorage["total_blocked"]);
+            
         }
     }
 }
 
 let bad_text = "Ad blockers are not allowed"
 
-//Sometimes a popup happens after a page is loaded, use an event listener
-document.addEventListener('DOMContentLoaded', function() {
-    block_popup_with_text(bad_text);
+//Sometimes a popup happens after a page is loaded, use a mutation observer. Credit: https://stackoverflow.com/questions/16618876/determining-if-a-html-element-has-been-added-to-the-dom-dynamically/16618904#16618904
+const observer = new MutationObserver(function (mutations){
+    //console.log("Page mutated!");
+    mutations.forEach(function (mutation){
+        if (mutation.addedNodes && mutation.addedNodes.length > 0){
+            block_popup_with_text(bad_text);
+        }
+    })
 })
 
+observer.observe(document, {
+    childList: true,
+    subtree: true,
+});
 
+
+
+//Testing purposes, run the function immedietly
+block_popup_with_text(bad_text);
+
+
+/* This code appears to work, but is deprecated. Using the MutationObserver instead.
+// I want to keep the code around just incade I come back to this
+
+document.addEventListener("DOMSubtreeModified", function () {
+  console.log("DOMSubtreeModified event detected.");
+  block_popup_with_text(bad_text);
+});
+*/
---
+++ popup.js
@@ -0,0 +1,11 @@
+
+let total_blocked_element = document.getElementById("blocked_count");
+let total_blocked = localStorage["total_blocked"];
+
+console.log(total_blocked);
+
+//Cant use local storage for this, going to have to use something else
+//total_blocked_element.textContent = "Total Blocked: " + total_blocked;
+
+
+
