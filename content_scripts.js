a342fd4697ff9317ed1dd5516ae3bd92956c280b
Wyatt H <jhawes@ucsc.edu>
2023-10-04 21:23:39 -0700

    Changing content_script and popup to use the service worker

--- content_scripts.js
+++ content_scripts.js
@@ -16,16 +16,9 @@ function block_popup_with_text(text_to_find){
             popup.remove();
             //console.log("Popup removed!")
 
+
             //Increment the number of blocked popups
-            let total_blocked = 0;
-            //returns null if item doesn't exist
-            if (localStorage["total_blocked"] != null){
-                total_blocked = parseInt(localStorage["total_blocked"])
-            }
-            total_blocked++;
-            localStorage["total_blocked"] = total_blocked;
-            console.log("Total Popups Blocked: " + localStorage["total_blocked"]);
-            
+            chrome.runtime.sendMessage({type: 'update counter'})
         }
     }
 }
--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
 
 let total_blocked_element = document.getElementById("blocked_count");
-let total_blocked = localStorage["total_blocked"];
 
-console.log(total_blocked);
 
 //Cant use local storage for this, going to have to use something else
 //total_blocked_element.textContent = "Total Blocked: " + total_blocked;
 
-
+// Send a message to the background script to request a counter update.
+chrome.runtime.sendMessage({ type: 'request counter' }, (response) => {
+    // Handle the response if needed.
+    if (response && response.type === 'counter response') {
+      // Update the counter display in your popup.
+      console.log("Updating counter")
+      total_blocked_element.textContent = "Total Blocked: " + response.value;
+    }
+  });
 
