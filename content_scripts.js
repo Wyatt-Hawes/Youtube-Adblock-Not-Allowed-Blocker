e6379e9df87cc48bc23a2b6e389dae32e23c4a8c
Wyatt H <jhawes@ucsc.edu>
2023-10-06 22:56:13 -0700

    Handling intentional pauses in a better way. Media keys, clicking the video, and clicking the pause button now actually work.

--- content_scripts.js
+++ content_scripts.js
@@ -1,5 +1,11 @@
 console.log("YAB not allowed script running");
 
+//Text that will be searched in popup windows. If ant text in the box matches the one below, it will be deleted
+const bad_text = "Ad blockers are not allowed"
+
+//Variable to store the currently playing video
+let playing_video = document.querySelector('video');
+
 function block_popup_with_text(text_to_find){
     // Grab all pop-up elements on the page
     const popup_elements = document.querySelectorAll('.style-scope.ytd-app');
@@ -14,24 +20,66 @@ function block_popup_with_text(text_to_find){
             //Increment the number of blocked popups
             chrome.runtime.sendMessage({type: 'update counter'})
 
-            //Video gets paused when popup occurs, time to unblock the video
+            //Video gets paused when popup occurs, time to unpause the video
             unpause_video();
         }
     }
 }
 
-let bad_text = "Ad blockers are not allowed"
+function unpause_video(){
+    //Maybe the video can change(?) so recalculate it.
+    playing_video = document.querySelector('video');
+
+    //Make sure a video actually exists/is not null
+    if (playing_video){
+        playing_video.play();
+        console.log("Video Resumed!")
+    }
+}
+
+
+//If the video is paused, determine if it was from a user-click or because it was paused automatically.
+playing_video.addEventListener('pause', function () {
+    console.log("Handling Pause");
+    handle_video_paused();
+})
+
+//Check if the user clicked on the video itself to pause
+add_click_pause_event_listener(playing_video);
+
+// Experimental: May have false positives causing the video not not resume when a popup happens
+// This is to hopefully fix the fact that the pause button that you click in the bottom left no longer works with the other setups
+// EDIT: This appears to only trigger when the click happens on the bottom left pause button (weird??) so I believe it wont have any false positives
+add_click_pause_event_listener(document);
+
+//Add a click event to the target that will correctly set the last click time and handle a video pause event.
+function add_click_pause_event_listener(target){
+    target.addEventListener('click', ()=>{
+        last_click = Date.now();
+        handle_video_paused();
+    });
+}
+
+//Check if the user clicked a Media pause key (like on the keyboard)
+document.addEventListener('keydown', (event)=>{
+    console.log("pressed!")
+    if (event.key === 'MediaPlayPause' || event.key === ' ' || event.key === "k") {
+        last_click = Date.now();
+        handle_video_paused();
+    }
+})
 
 
 
-/* This code appears to have worked, but the eventListener and DOMSubtreeModifed are deprecated. Using the MutationObserver instead.
-// I want to keep the code around just incase I come back to this
+let last_click = 0;
+function handle_video_paused(){
+    const time_elapsed_since_last_click = Date.now() - last_click; //Time in milliseconds
+    if (Math.abs(time_elapsed_since_last_click) > 300){
+        unpause_video();
+    }
+    //Else: Do nothing, let the video get paused since it was most likely a user click pausing the video
+}
 
-document.addEventListener("DOMSubtreeModified", function () {
-  console.log("DOMSubtreeModified event detected.");
-  block_popup_with_text(bad_text);
-});
-*/
 //Sometimes a popup happens after a page is loaded, use a mutation observer. Credit: https://stackoverflow.com/questions/16618876/determining-if-a-html-element-has-been-added-to-the-dom-dynamically/16618904#16618904
 const observer = new MutationObserver(function (mutations){
     //Page mutated!
@@ -42,6 +90,7 @@ const observer = new MutationObserver(function (mutations){
     })
 })
 
+//Set the observer to observe the page
 observer.observe(document, {
     childList: true,
     subtree: true,
@@ -52,26 +101,25 @@ observer.observe(document, {
 //Testing purposes, run the function immedietly
 block_popup_with_text(bad_text);
 
-//Select the playing video
-const playing_video = document.querySelector('video');
 
-function unpause_video(){
-    console.log("Attempting Video Unpause")
-    //Make sure a video actually exists/is not null
-    if (playing_video){
-        playing_video.play();
-        console.log("Video Resumed")
-    }
-}
-
+//----------------------------------------------------------------------
 /*
 //Testing purposes, add a testing key.
-//Unpause video successfully works!
+//Edit: Unpauses the youtube video successfully!
 
 document.addEventListener("keydown", function (event) {
     console.log("Key pressed!")
     if (event.key === "`" || event.key === "~"){
         unpause_video();
     }
+});
+*/
+
+/* This code appears to have worked, but the eventListener and DOMSubtreeModifed are deprecated. Using the MutationObserver instead.
+// I want to keep the code around just incase I come back to this
+
+document.addEventListener("DOMSubtreeModified", function () {
+  console.log("DOMSubtreeModified event detected.");
+  block_popup_with_text(bad_text);
 });
 */
