03186b917134474b1d3d3ff114ce67ee3de8d786
Wyatt H <jhawes@ucsc.edu>
2023-10-06 17:07:26 -0700

    Adding functionality: Video resumes after block pauses it

--- content_scripts.js
+++ content_scripts.js
@@ -13,12 +13,25 @@ function block_popup_with_text(text_to_find){
 
             //Increment the number of blocked popups
             chrome.runtime.sendMessage({type: 'update counter'})
+
+            //Video gets paused when popup occurs, time to unblock the video
+            unpause_video();
         }
     }
 }
 
 let bad_text = "Ad blockers are not allowed"
 
+
+
+/* This code appears to have worked, but the eventListener and DOMSubtreeModifed are deprecated. Using the MutationObserver instead.
+// I want to keep the code around just incase I come back to this
+
+document.addEventListener("DOMSubtreeModified", function () {
+  console.log("DOMSubtreeModified event detected.");
+  block_popup_with_text(bad_text);
+});
+*/
 //Sometimes a popup happens after a page is loaded, use a mutation observer. Credit: https://stackoverflow.com/questions/16618876/determining-if-a-html-element-has-been-added-to-the-dom-dynamically/16618904#16618904
 const observer = new MutationObserver(function (mutations){
     //Page mutated!
@@ -39,12 +52,26 @@ observer.observe(document, {
 //Testing purposes, run the function immedietly
 block_popup_with_text(bad_text);
 
+//Select the playing video
+const playing_video = document.querySelector('video');
 
-/* This code appears to have worked, but the eventListener and DOMSubtreeModifed are deprecated. Using the MutationObserver instead.
-// I want to keep the code around just incase I come back to this
+function unpause_video(){
+    console.log("Attempting Video Unpause")
+    //Make sure a video actually exists/is not null
+    if (playing_video){
+        playing_video.play();
+        console.log("Video Resumed")
+    }
+}
 
-document.addEventListener("DOMSubtreeModified", function () {
-  console.log("DOMSubtreeModified event detected.");
-  block_popup_with_text(bad_text);
+/*
+//Testing purposes, add a testing key.
+//Unpause video successfully works!
+
+document.addEventListener("keydown", function (event) {
+    console.log("Key pressed!")
+    if (event.key === "`" || event.key === "~"){
+        unpause_video();
+    }
 });
 */
